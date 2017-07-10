6d31d40500744a6027f34923b1bac3ac248f992e
BInita Technoadviser <binita.modi@technoadviser.com>
2017-07-10 13:09:24 +0530

    Test application

---
+++ AppTest/AppTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AppTest
+//
+//  Created by Apple on 10/07/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
